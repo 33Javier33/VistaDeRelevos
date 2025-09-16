6512aebecaa1bca76f7110d182a297f87f0c752f
33Javier33 <c.javier.p.nauto@gmail.com>
2025-09-16 09:31:32 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'horarios-viewer-cache-v1';
+const CACHE_NAME = 'horarios-viewer-cache-v2'; // Versión actualizada para forzar la actualización
 const urlsToCache = [
-  './vista.html'
+  './index.html'
 ];
 
 self.addEventListener('install', event => {
@@ -13,19 +13,34 @@ self.addEventListener('install', event => {
   );
 });
 
+// Limpiar caches antiguas
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
 self.addEventListener('fetch', event => {
+  // Ignorar las peticiones a Google Scripts para que siempre vaya a la red
   if (event.request.url.includes('script.google.com')) {
+    event.respondWith(fetch(event.request));
     return;
   }
 
+  // Estrategia "Network falling back to cache" para el resto
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
