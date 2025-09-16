7fbc5a70ad53f4cfdbeeff6b3fd32a108ac7a4c3
33Javier33 <c.javier.p.nauto@gmail.com>
2025-09-16 08:37:18 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'horarios-viewer-cache-v1';
+const urlsToCache = [
+  './vista.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierta');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('script.google.com')) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
